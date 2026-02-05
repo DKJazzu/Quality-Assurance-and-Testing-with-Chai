f4bc01f43e9229ba3a236dc73a04610914737a38
DKJazzu <dkjazzu@gmail.com>
2026-02-05 09:34:09 +0200

    Add files via upload

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -56,7 +56,7 @@ suite('Functional Tests', function () {
         .put('/travellers')
         .send({ surname: 'da Verrazzano' })
         .end(function(err, res) {
-          assert.equal(res.body, 200);
+          assert.equal(res.status, 200);
           assert.equal(res.type, 'application/json');
           assert.equal(res.body.name, 'Giovanni');
           assert.equal(res.body.surname, 'da Verrazzano');
